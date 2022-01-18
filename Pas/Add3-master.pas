4c4739275916fa926be079325093a476f4491971
bwattle <neville.goodyer@det.nsw.edu.au>
2022-01-18 21:08:18 +1100

    index and other files updated

--- Pas/Add3-master.pas
+++ Pas/Add3-master.pas
@@ -4,11 +4,11 @@ Var
   number_1, number_2, number_3, total: String;      {declarations}
 Begin      
   Writeln('v1.01 Add3. You will be asked for 3 numbers:'); {version}
-  Writeln('Enter first number');                  {read user inputs}
+  Writeln('Enter first number:');                  {read user inputs}
   Readln(number_1);
-  Writeln('Enter second number');
+  Writeln('Enter second number:');
   Readln(number_2);
-  Writeln('Enter third number');
+  Writeln('Enter third number:');
   Readln(number_3);
   total := number_1 + number_2 + number_3;          {assignment}
   Writeln('The total is ', total);                  {dispaly}
--- Py/Add3-v101-Concat.py
+++ Py/Add3-v101-Concat.py
@@ -1,7 +1,7 @@
 # add three numbers
 print("v1.01 Add3. You will be asked for 3 numbers:")   #version number
-Number_1 = input("Enter your first # : ")               #read user inputs
-Number_2 = input("Enter your second # : ")
-Number_3 = input("Enter your third # : ")
-total = Number_1 + Number_2 + Number_3                  #assignment step
+number_1 = input("Enter your first number: ")               #read user inputs
+number_2 = input("Enter your second number: ")
+number_3 = input("Enter your third number: ")
+total = number_1 + number_2 + number_3                  #assignment step
 print("The sum is ", total)                             #display step

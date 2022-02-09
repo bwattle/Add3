dcceb4166a12aefd888873c832c7b624271c8ea5
Nev Goodyer <neville.goodyer@det.nsw.edu.au>
2022-02-09 16:23:03 +1100

    Changes to the index and some files

--- VB-master/Add3.vb
+++ VB-master/Add3.vb
@@ -1,11 +1,11 @@
 ﻿Public Class Add3
-	Private Number_1, Number_2, Number_3 As String  'this is a deliberate error
+	Private number_1, number_2, number_3 As String  'this is a deliberate error
 	Private total As Integer                        'declaration of variables
 	Private Sub BtnCalcSum_Click(sender As Object, e As EventArgs) Handles BtnCalcSum.Click
-		Number_1 = txtNumber_1.Text                 'read user inputs
-		Number_2 = txtNumber_2.Text
-		Number_3 = txtNumber_3.Text
-		total = Number_1 + Number_2 + Number_3      'assignment step
+		number_1 = txtNumber_1.Text                 'read user inputs
+		number_2 = txtNumber_2.Text
+		number_3 = txtNumber_3.Text
+		total = number_1 + number_2 + number_3      'assignment step
 		txtTotalSum.Text = total                    'display step
 	End Sub
 End Class
